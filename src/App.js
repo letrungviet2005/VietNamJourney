6c08b70b9a0e1581e0825c082f9eeeebecc7e924
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-06-09 21:09:35 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar.js';
-import { Routes, Route } from 'react-router-dom';
-import Intro from './Intro/Intro.js'
-import TrangChu from './TrangChu/TrangChu.js'
-import CongDong from './CongDong/CongDong.js'
-import ChienDich from './ChienDich/ChienDich.js'
-import Quy from './Quy/Quy.js'
-import TaiKhoan from './TaiKhoan/TaiKhoan.js'
-import User from './User/User.js'
+import Intro from './Intro/Intro.js';
+import TrangChu from './TrangChu/TrangChu.js';
+import CongDong from './CongDong/CongDong.js';
+import ChienDich from './ChienDich/ChienDich.js';
+import Quy from './Quy/Quy.js';
+import TaiKhoan from './TaiKhoan/TaiKhoan.js';
+import User from './User/User.js';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div style={{ height: '3.8rem' }}></div>
-      <Routes>
-      <Route path="/VietNamJourney/" element={<Intro />} />
-      <Route path="/VietNamJourney/TrangChu" element={<TrangChu />} />
-      <Route path="/VietNamJourney/CongDong" element={<CongDong />} />
-      <Route path="/VietNamJourney/ChienDich" element={<ChienDich />} />
-      <Route path="/VietNamJourney/Quy" element={<Quy />} />
-      <Route path="/VietNamJourney/TaiKhoan" element={<TaiKhoan />} />
-      <Route path="/VietNamJourney/User" element={<User />} />
-      {/* Thêm các Route khác ở đây */}
-      </Routes>
-    </div>
-    
+    <Router basename="/VietNamJourney">
+      <div>
+        <Navbar />
+        <div style={{ height: '3.8rem' }}></div>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/TrangChu" element={<TrangChu />} />
+          <Route path="/CongDong" element={<CongDong />} />
+          <Route path="/ChienDich" element={<ChienDich />} />
+          <Route path="/Quy" element={<Quy />} />
+          <Route path="/TaiKhoan" element={<TaiKhoan />} />
+          <Route path="/User" element={<User />} />
+          {/* Thêm các Route khác ở đây */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
