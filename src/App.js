aaa8068b9f19f1df6d38e7ba9b78edfe1b2497ae
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-06-10 11:35:12 +0700

    mnew

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from './component/Navbar/Navbar.js';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Intro from './pages/Intro/Intro.js'
 import TrangChu from './pages/TrangChu/TrangChu.js'
 import CongDong from './pages/CongDong/CongDong.js'
@@ -10,32 +9,22 @@ import TaiKhoan from './pages/TaiKhoan/TaiKhoan.js'
 import User from './pages/User/User.js'
 
 function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const redirect = params.get('redirect');
-    if (redirect && location.pathname === '/') {
-      navigate(redirect);
-    }
-  }, [navigate, location]);
-
   return (
     <div>
       <Navbar />
       <div style={{ height: '3.8rem' }}></div>
       <Routes>
-        <Route path="/VietNamJourney/" element={<Intro />} />
-        <Route path="/VietNamJourney/TrangChu" element={<TrangChu />} />
-        <Route path="/VietNamJourney/CongDong" element={<CongDong />} />
-        <Route path="/VietNamJourney/ChienDich" element={<ChienDich />} />
-        <Route path="/VietNamJourney/Quy" element={<Quy />} />
-        <Route path="/VietNamJourney/TaiKhoan" element={<TaiKhoan />} />
-        <Route path="/VietNamJourney/User" element={<User />} />
-        {/* Thêm các Route khác ở đây */}
+      <Route path="/VietNamJourney/" element={<Intro />} />
+      <Route path="/VietNamJourney/TrangChu" element={<TrangChu />} />
+      <Route path="/VietNamJourney/CongDong" element={<CongDong />} />
+      <Route path="/VietNamJourney/ChienDich" element={<ChienDich />} />
+      <Route path="/VietNamJourney/Quy" element={<Quy />} />
+      <Route path="/VietNamJourney/TaiKhoan" element={<TaiKhoan />} />
+      <Route path="/VietNamJourney/User" element={<User />} />
+      {/* Thêm các Route khác ở đây */}
       </Routes>
     </div>
+    
   );
 }
 
