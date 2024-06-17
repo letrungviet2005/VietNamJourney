0223a2ded562280a78af81eea5985f832ab40e7d
Boima <Boimache27@gmail.com>
2024-06-17 17:05:52 +0700

    Quy SoLuoc

--- src/pages/Quy/Quy.js
+++ src/pages/Quy/Quy.js
@@ -1,3 +1,4 @@
+import CoSoLuoc from "./SoLuoc/SoLuoc";
 import CoSuMenh1 from "./SuMenh1/SuMenh1";
 import CoSuMenh2 from "./SuMenh2/SuMenh2";
 
@@ -12,7 +13,7 @@ function Quy() {
     return (
         <div>
             
-
+            <CoSoLuoc />
             <CoSuMenh1 />
             <CoSuMenh2 />
 
