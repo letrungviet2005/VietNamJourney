766bdd7ca32f0295a485f8f8382db80bfdc4cfe5
Boima <Boimache27@gmail.com>
2024-06-18 07:24:59 +0700

    Quy Donate

--- src/pages/Quy/Quy.js
+++ src/pages/Quy/Quy.js
@@ -2,7 +2,7 @@ import CoSoLuoc from "./SoLuoc/SoLuoc";
 import CoSuMenh1 from "./SuMenh1/SuMenh1";
 import CoSuMenh2 from "./SuMenh2/SuMenh2";
 import CoCauChuyen from "./CauChuyen/CauChuyen";
-
+import CoDonate from "./Donate/Donate";
 
 
 
@@ -17,7 +17,7 @@ function Quy() {
             <CoSuMenh1 />
             <CoSuMenh2 />
             <CoCauChuyen />
-
+            <CoDonate />
 
         </div>
     );
