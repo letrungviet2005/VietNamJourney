2ae67719a0ad7ebb02e9ec9f31250507eb7baa19
Boima <Boimache27@gmail.com>
2024-06-21 08:59:44 +0700

    TrangChu Responsive for 1536px

--- src/pages/TrangChu/ThucTrangVideo/ThucTrangVideo.jsx
+++ src/pages/TrangChu/ThucTrangVideo/ThucTrangVideo.jsx
@@ -1,7 +1,11 @@
+import styles from './ThucTrangVideo.module.css';
+
+
+
 function CoThucTrangVideo() {
     return(
-        <div>
-            <iframe width="1512" height="850" 
+        <div className={styles.main}>
+            <iframe className={styles.vid} 
             src="https://www.youtube.com/embed/JOetIicQhHs?si=w6nIxFj5nM2coV7A"
             title="A youtube video"
             ></iframe>
