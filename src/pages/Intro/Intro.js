09cde3f3ef0d153a562f38c8f8ede98082b0eaf1
Boima <Boimache27@gmail.com>
2024-06-22 08:34:48 +0700

    Intro

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Intro.module.css';
+import { Link } from 'react-router-dom';
 
-import logo from "../../Images/Intro/logo.png";
 import playbt from "../../Images/Intro/playbt.png";
 import vid1 from "../../Images/Intro/videointro.mp4";
 
-
-
 function Intro() {
+    const videoRef = useRef(null);
+    const [isMidVisible, setIsMidVisible] = useState(true);
+
+    const playVid = () => {
+        if (videoRef.current) {
+            videoRef.current.play();
+        }
+        setIsMidVisible(false);
+    };
+
     return (
         <div className={styles.main}>
-
             <div className={styles.vid}>
-                <video controls>
-                    <source src={vid1} type="video/mp4"></source>
-
-                    Your browser doesn't support the video tag
+                <video id='myvideo' ref={videoRef}>
+                    <source src={vid1} type="video/mp4" />
+                    Your browser doesn't support the video tag.
                 </video>
             </div>
-
             <div className={styles.front}>
-                <img alt="a" src={logo} className={styles.logo}></img>
-
-                <div className={styles.mid}>
-                    <p>Bạn đang chần chờ gì<br></br> hãy đi cùng chúng tôi</p>
-
-                    <img alt="a" src={playbt}></img>
-                </div>
-
-                <p className={styles.next}>Tiếp Tục</p>
+                {isMidVisible && (
+                    <div className={styles.mid}>
+                        <p>Bạn đang chần chờ gì<br /> hãy đi cùng chúng tôi</p>
+                        <img alt="a" src={playbt} onClick={playVid} />
+                    </div>
+                )}
+                <Link className={styles.lnk} to="/TrangChu">
+                    <p className={styles.next}>Tiếp Tục</p>
+                </Link>
             </div>
-
         </div>
     );
 }
-
 
 export default Intro;
