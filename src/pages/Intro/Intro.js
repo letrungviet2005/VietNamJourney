83b4105b27c14916ce7e52ad98a74abfcb5934bc
Boima <Boimache27@gmail.com>
2024-07-03 11:49:24 +0700

    setup for videos)

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,40 +1,27 @@
-import React, { useRef, useState } from 'react';
 import styles from './Intro.module.css';
 import { Link } from 'react-router-dom';
 
-import playbt from "../../Images/Intro/playbt.png";
-// import vid1 from "../../Images/Intro/videointro.mp4";
+import vid1 from "../../videos/videointro.mp4";
 
 function Intro() {
-    const videoRef = useRef(null);
-    const [isMidVisible, setIsMidVisible] = useState(true);
-
-    const playVid = () => {
-        if (videoRef.current) {
-            videoRef.current.play();
-        }
-        setIsMidVisible(false);
-    };
 
     return (
         <div className={styles.main}>
             <div className={styles.vid}>
-                {/* <video id='myvideo' ref={videoRef}>
+                <video id='myvideo' controls>
                     <source src={vid1} type="video/mp4" />
                     Your browser doesn't support the video tag.
-                </video> */}
+                </video>
+
             </div>
-            <div className={styles.front}>
-                {isMidVisible && (
-                    <div className={styles.mid}>
-                        <p>Bạn đang chần chờ gì<br /> hãy đi cùng chúng tôi</p>
-                        <img alt="a" src={playbt} onClick={playVid} />
-                    </div>
-                )}
+
+            {/* <div className={styles.front}>
                 <Link className={styles.lnk} to="/TrangChu">
                     <p className={styles.next}>Tiếp Tục</p>
                 </Link>
-            </div>
+
+            </div> */}
+
         </div>
     );
 }
